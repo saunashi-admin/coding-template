a20906811306d3e32683f1a599ab5204a7bcd428
suehiro <suehiro@saunashi.com>
2021-02-08 12:22:47 +0900

    pug,scss,jsテンプレート修正

--- src/js/intersectionObserver.js
+++ src/js/intersectionObserver.js
@@ -2,115 +2,76 @@
  * IE11 polyfill
  * Intersection ObserverはIE以外はデフォルトで使える
  */
-//polyfill使わないときはコメントアウト
+// polyfill使わないときはコメントアウト
 import 'intersection-observer';
+
 IntersectionObserver.prototype.POLL_INTERVAL = 100; // Time in milliseconds.
-
-// function onIntersect(entries, observer) {
-//   console.log(entries);
-//   //交差検知したもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
-//   entries.forEach((entry) => {
-//     //entry.isIntersectingで発火。オブジェクト確認
-//     if (entry.isIntersecting) {
-//       activateIndex(entry.target);
-//     }
-//   });
-// }
-
-// function activateIndex(element) {
-//   // すでにアクティブになっている目次を選択
-//   const currentActiveIndex = document.querySelector(
-//     ".Header_nav_list .-active"
-//   );
-//   // すでにアクティブになっているものが0個（=null）の時以外は、activeクラスを除去
-//   // currentActiveIndex !== null でもOK
-//   if (currentActiveIndex) {
-//     currentActiveIndex.classList.remove("-active");
-//   }
-//   // 引数で渡されたDOMが飛び先のaタグを選択し、activeクラスを付与
-//   const newActiveIndex = document.querySelector(
-//     `.Header_nav_list a[href="#${element.id}"]`
-//   );
-//   if (newActiveIndex) {
-//     newActiveIndex.classList.add("-active");
-//   }
-// }
-
-// const observer = new IntersectionObserver(onIntersect, {
-//   root: null, //ビューポート
-//   rootMargin: "-50% 0px", //0でもpxつける,-50%で要素の中央で発火
-//   threshold: 0,
-// });
-
-// //交差を監視する要素
-// const targets = document.querySelectorAll(".js-obserbed");
-// // それぞれの要素を監視する
-// if (targets) {
-//   targets.forEach((target) => {
-//     observer.observe(target);
-//   });
-// }
 
 /*
  * lazy load
  */
-// const lazyImages = document.querySelectorAll(".lazy-img");
-const lazyImages = document.querySelectorAll('[data-lazyloaded]');
-const lazyImageObserver = new IntersectionObserver(onIntersect, {
-  root: null, //ビューポート
-  rootMargin: '200px 0px', //0でもpxつける,-50%で要素の中央で発火
-  threshold: 0,
-});
-
-function onIntersect(entries, observer) {
-  console.log(entries);
-  //交差検知したもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
-  entries.forEach((entry) => {
-    //entry.isIntersectingで発火。オブジェクト確認
-    if (entry.isIntersecting) {
-      const imgElement = entry.target;
-      const imgElementTagName = imgElement.tagName;
-      if (imgElementTagName == 'IMG') {
-        imgElement.src = imgElement.dataset.src;
-      } else if (imgElementTagName == 'PICTURE') {
-        imgElement.srcset = imgElement.dataset.src;
+const lazyImageObserver = () => {
+  const targets = document.querySelectorAll('[data-lazyloaded]');
+  const onIntersect = (entries, observer) => {
+    // 交差検知したもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
+    entries.forEach((entry) => {
+      // entry.isIntersectingで発火。オブジェクト確認
+      if (entry.isIntersecting) {
+        const imgElement = entry.target;
+        const imgElementTagName = imgElement.tagName;
+        if (imgElementTagName === 'IMG') {
+          imgElement.src = imgElement.dataset.src;
+        } else if (imgElementTagName === 'PICTURE') {
+          imgElement.srcset = imgElement.dataset.src;
+        }
+        imgElement.addEventListener('load', () => {
+          imgElement.dataset.lazyloaded = true;
+        });
+        observer.unobserve(imgElement);
       }
-      imgElement.addEventListener('load', () => {
-        imgElement.dataset.lazyloaded = true;
-      });
-      lazyImageObserver.unobserve(imgElement);
-    }
+    });
+  };
+  const observer = new IntersectionObserver(onIntersect, {
+    root: null, // ビューポート
+    rootMargin: '200px 0px', // 0でもpxつける,-50%で要素の中央で発火
+    threshold: 0,
   });
-}
-if (lazyImages) {
-  lazyImages.forEach((lazyImage) => {
-    lazyImageObserver.observe(lazyImage);
-  });
-}
+
+  if (targets) {
+    targets.forEach((target) => {
+      observer.observe(target);
+    });
+  }
+};
+lazyImageObserver();
 
 /*
  *  アニメーション
  */
-const effectTargets = document.querySelectorAll('.effect-target');
-const effectTargetsObserver = new IntersectionObserver(onIntersectEffect, {
-  root: null, //ビューポート
-  rootMargin: '0px 0px', //0でもpxつける,-50%で要素の中央で発火
-  threshold: 0,
-});
-function onIntersectEffect(entries, observer) {
-  console.log(entries);
-  //交差検知したもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
-  entries.forEach((entry) => {
-    //entry.isIntersectingで発火。オブジェクト確認
-    if (entry.isIntersecting) {
-      const target = entry.target;
-      target.classList.add('-action');
-      effectTargetsObserver.unobserve(target);
-    }
+const effectObserver = () => {
+  const targets = document.querySelectorAll('[data-effect]');
+  const observer = new IntersectionObserver(onIntersect, {
+    root: null, // ビューポート
+    rootMargin: '0px 0px', // 0でもpxつける,-50%で要素の中央で発火
+    threshold: 0,
   });
-}
-if (effectTargets) {
-  effectTargets.forEach((effectTarget) => {
-    effectTargetsObserver.observe(effectTarget);
-  });
-}
+  function onIntersect(entries, observer) {
+    // console.log(entries);
+    // 交差検知したもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
+    entries.forEach((entry) => {
+      // entry.isIntersectingで発火。オブジェクト確認
+      if (entry.isIntersecting) {
+        const { target } = entry;
+        // target.classList.add('-action');
+        target.dataset.effect = 'fired';
+        observer.unobserve(target);
+      }
+    });
+  }
+  if (targets) {
+    targets.forEach((target) => {
+      observer.observe(target);
+    });
+  }
+};
+effectObserver();
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,54 @@
-"use strict";
+// scssコンパイルのために必須の記述
+// import "../scss/style.scss";
 
-import "./intersectionObserver";
-import "./smoothScroll";
-// import noScrollToggle from "./noScrollToggle";
-import "@babel/polyfill";
+// js import
+import './intersectionObserver';
+import './smoothScroll';
+import '@babel/polyfill';
+// import noScrollToggle from './noscroll';
+import './menu';
+import resetHeader from './menu';
+import './topTop';
+import './externalLink';
 
-//ポリフィル
-import objectFitImages from "object-fit-images";
+// ポリフィル
+import objectFitImages from 'object-fit-images';
 objectFitImages();
 
+// ie識別・対策
+// const agent = window.navigator.userAgent.toLowerCase();
+// const isIE11 = agent.indexOf('trident/7') !== -1;
+
+// 別ページからのアンカーずれ解消
+document.addEventListener('DOMContentLoaded', () => {
+  if (location.hash) {
+    const headerHeight = document.getElementById('js-header').clientHeight;
+    setTimeout(() => {
+      window.scrollBy(0, -headerHeight);
+    }, 100);
+  }
+});
+
+/*
+ *ロード画面
+ */
+// if (document.getElementById('js-loading')) {
+//   const loadingFnc = (delay) => {
+//     const loading = document.getElementById('js-loading');
+//     window.addEventListener('load', function () {
+//       loading.classList.add('is-fadeout'); //_js.scss/is-fadeoutのdurationと合わせる
+//       this.setTimeout(() => {
+//         loading.style.display = 'none';
+//       }, delay);
+//     });
+//   };
+//   loadingFnc(1000);
+// }
+
 // メディアクエリ
-const mediaQueryList = window.matchMedia("(max-width:959px)");
+const mediaQueryList = window.matchMedia('(max-width:959px)');
 const listener = (e) => {
-	resetHeader(e);
+  resetHeader(e);
 };
 mediaQueryList.addListener(listener); // リスナー登録
 listener(mediaQueryList); // 初期ロード時
